ea7880f86d04e74f7093172ba48c619cd28573c3
wujihua <wjh50940@163.com>
2023-02-22 11:11:09 +0800

    fix: PDF builder

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,2 @@
 import './styles/index.scss'
 import './resume.md'
-
-const printButton = document.getElementById('print-button') as HTMLElement
-
-printButton.addEventListener('click', (event) => {
-  event.preventDefault()
-  window.print()
-})
